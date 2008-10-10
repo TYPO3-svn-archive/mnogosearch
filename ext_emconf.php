ed22a59928edd3098ac45d103fcbc5a696f47f11
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-10-10 07:28:54 +0000

    1.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mnogosearch/trunk@12907 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "mnogosearch"
 #
-# Auto generated 10-03-2008 17:05
+# Auto generated 09-10-2008 17:08
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -17,8 +17,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author' => 'Dmitry Dulepov',
 	'author_email' => 'dmitry@typo3.org',
 	'shy' => '',
-	'dependencies' => 'cms,lang',
-	'conflicts' => '',
+	'dependencies' => '',
+	'conflicts' => 'realurl,tstidy',
 	'priority' => '',
 	'module' => '',
 	'state' => 'stable',
@@ -29,20 +29,20 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'SIA "ACCIO"',
-	'version' => '1.0.2',
+	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'cms' => '',
-			'lang' => '',
 			'typo3' => '4.1.1-0.0.0',
+			'php' => '5.1.0-0.0.0',
 		),
 		'conflicts' => array(
 			'realurl' => '0.0.0-0.999.999',
+			'tstidy' => '',
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:32:{s:9:"ChangeLog";s:4:"672c";s:35:"class.tx_mnogosearch_cms_layout.php";s:4:"5e38";s:33:"class.tx_mnogosearch_postproc.php";s:4:"5035";s:32:"class.tx_mnogosearch_tcemain.php";s:4:"3b41";s:21:"ext_conf_template.txt";s:4:"a121";s:12:"ext_icon.gif";s:4:"208b";s:17:"ext_localconf.php";s:4:"75b9";s:14:"ext_tables.php";s:4:"e905";s:14:"ext_tables.sql";s:4:"db02";s:35:"icon_tx_mnogosearch_indexconfig.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"5931";s:7:"tca.php";s:4:"fd38";s:25:"cli/cli_mnogosearch.phpsh";s:4:"b003";s:12:"cli/conf.php";s:4:"1cdd";s:14:"doc/manual.sxw";s:4:"c3ed";s:32:"pi1/class.tx_mnogosearch_pi1.php";s:4:"e65c";s:36:"pi1/class.tx_mnogosearch_results.php";s:4:"c080";s:27:"pi1/ds_long_search_form.xml";s:4:"7d2b";s:25:"pi1/ds_search_results.xml";s:4:"1cdc";s:28:"pi1/ds_short_search_form.xml";s:4:"d825";s:19:"pi1/flexform_ds.xml";s:4:"d56e";s:17:"pi1/locallang.xml";s:4:"62e8";s:47:"pi1/renderers/class.tx_mnogosearch_renderer.php";s:4:"4261";s:51:"pi1/renderers/class.tx_mnogosearch_renderer_mtb.php";s:4:"1329";s:59:"pi1/renderers/class.tx_mnogosearch_renderer_templavoila.php";s:4:"74cb";s:29:"pi1/templates/search_form.css";s:4:"d41d";s:30:"pi1/templates/search_form.html";s:4:"e0d3";s:20:"pi1/templates/tv.css";s:4:"c9f7";s:21:"pi1/templates/tv.html";s:4:"fe31";s:29:"pi1/templates/tv_exported.xml";s:4:"77c0";s:32:"static/mnoGoSearch/constants.txt";s:4:"29b4";s:28:"static/mnoGoSearch/setup.txt";s:4:"165f";}',
+	'_md5_values_when_last_written' => 'a:31:{s:9:"ChangeLog";s:4:"af04";s:35:"class.tx_mnogosearch_cms_layout.php";s:4:"a98d";s:33:"class.tx_mnogosearch_postproc.php";s:4:"fc25";s:32:"class.tx_mnogosearch_tcemain.php";s:4:"2edb";s:21:"ext_conf_template.txt";s:4:"6583";s:12:"ext_icon.gif";s:4:"208b";s:17:"ext_localconf.php";s:4:"3b6e";s:14:"ext_tables.php";s:4:"f43a";s:14:"ext_tables.sql";s:4:"19e2";s:35:"icon_tx_mnogosearch_indexconfig.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"540b";s:7:"tca.php";s:4:"fb72";s:23:"cli/cli_mnogosearch.php";s:4:"34a6";s:14:"doc/manual.sxw";s:4:"048d";s:32:"pi1/class.tx_mnogosearch_pi1.php";s:4:"279d";s:36:"pi1/class.tx_mnogosearch_results.php";s:4:"d2ef";s:27:"pi1/ds_long_search_form.xml";s:4:"7d2b";s:25:"pi1/ds_search_results.xml";s:4:"1cdc";s:28:"pi1/ds_short_search_form.xml";s:4:"d825";s:19:"pi1/flexform_ds.xml";s:4:"d56e";s:17:"pi1/locallang.xml";s:4:"62e8";s:47:"pi1/renderers/class.tx_mnogosearch_renderer.php";s:4:"4261";s:51:"pi1/renderers/class.tx_mnogosearch_renderer_mtb.php";s:4:"03d0";s:59:"pi1/renderers/class.tx_mnogosearch_renderer_templavoila.php";s:4:"74cb";s:29:"pi1/templates/search_form.css";s:4:"d41d";s:30:"pi1/templates/search_form.html";s:4:"e6ff";s:20:"pi1/templates/tv.css";s:4:"c9f7";s:21:"pi1/templates/tv.html";s:4:"fe31";s:29:"pi1/templates/tv_exported.xml";s:4:"77c0";s:32:"static/mnoGoSearch/constants.txt";s:4:"29b4";s:28:"static/mnoGoSearch/setup.txt";s:4:"165f";}',
 	'suggests' => array(
 	),
 );
