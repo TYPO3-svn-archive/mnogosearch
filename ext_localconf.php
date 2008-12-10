9372385865346aedaf7450107eb3142b241f99e3
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-10 15:30:40 +0000

    Added tt_news hook to set page modification date correctly
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mnogosearch/trunk@14803 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,7 +6,10 @@ t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_mnogosearch_pi1.php', '_pi1', '
 // Register hook only if our header is present
 if (TYPO3_MODE == 'FE' && $_SERVER['HTTP_X_TYPO3_MNOGOSEARCH'] == md5('mnogosearch' . $GLOBALS['TYPO3_CONF_VARS']['SYS']['encryptionKey'])) {
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output']['mnogosearch'] = 'EXT:mnogosearch/class.tx_mnogosearch_postproc.php:tx_mnogosearch_postproc->contentPostProcOutput';
+
 }
+// Hook for tt_news to set page modification date correctly. This must run outside of the above block because page can be cached and it will not be executed when necessary
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tt_news']['extraItemMarkerHook'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/hooks/class.tx_mnogosearch_ttnews.php:&tx_mnogosearch_ttnews';
 
 if (TYPO3_MODE == 'BE') {
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass']['mnogosearch'] = 'EXT:mnogosearch/class.tx_mnogosearch_tcemain.php:tx_mnogosearch_tcemain';
