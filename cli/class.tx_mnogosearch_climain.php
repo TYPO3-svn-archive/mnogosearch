a2abdc0118ea050fd9a7289d0804cdbd2472e683
fedir <fedir@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-07-04 10:22:24 +0000

    Bug #27827
    sys_get_temp_dir for PHP older thank 5.2.1
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mnogosearch/trunk@49394 735d13b6-9817-0410-8766-e36946ffe9aa

--- cli/class.tx_mnogosearch_climain.php
+++ cli/class.tx_mnogosearch_climain.php
@@ -33,6 +33,20 @@ if (!defined('TYPO3_MODE')) {
 	exit;
 }
 
+if ( !function_exists('sys_get_temp_dir')) {
+	function sys_get_temp_dir() {
+		if( $temp=getenv('TMP') )   	return $temp;
+		if( $temp=getenv('TEMP') )      return $temp;
+		if( $temp=getenv('TMPDIR') )    return $temp;
+		$temp=tempnam(__FILE__,'');
+		if (file_exists($temp)) {
+			unlink($temp);
+			return dirname($temp);
+		}
+		return null;
+	}
+}
+
 require_once(t3lib_extMgm::extPath('mnogosearch', 'cli/class.tx_mnogosearch_clioptions.php'));
 require_once(t3lib_extMgm::extPath('mnogosearch', 'cli/class.tx_mnogosearch_configgenerator.php'));
 require_once(t3lib_extMgm::extPath('mnogosearch', 'cli/class.tx_mnogosearch_dboperations.php'));
